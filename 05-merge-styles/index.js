9bcb2683dab20784e27cec508a620ca674ced3d8
Aleksey <AlekseyVY@gmail.com>
2021-11-06 12:02:38 +0300

    additional fix

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,19 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 
+
 const buildCSS = async () => {
-  const files = await fs.promises.readdir(path.join(__dirname, 'styles'));
-  let template = ''
+  const files = await fs.promises.readdir(path.join(__dirname, 'styles'), {withFileTypes: true});
+  const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), {flags: 'w'})
+  let template = []
   await Promise.all(files.map(file => new Promise(async (res) => {
-    await fs.stat(path.join(__dirname, 'styles', file), async (err, stats) => {
-      if(!stats.isDirectory() && path.extname(path.join(__dirname, 'styles', file)) === '.css') {
-        const chunk = await fs.promises.readFile(path.join(__dirname, 'styles', file), {encoding: 'utf-8'})
-        template += chunk
+      if(file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css') {
+        const chunk = await fs.promises.readFile(path.join(__dirname, 'styles', file.name), {encoding: 'utf-8'})
+        template.push(chunk)
+        writeStream.write(chunk);
+        writeStream.write('\n');
         res();
       }
-    });
   })))
-  await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), template,{flags: 'w'})
 }
 
 buildCSS()
