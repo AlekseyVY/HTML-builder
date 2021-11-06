30a7b9d30e309312b8886028be4947475f2b39cc
Aleksey <AlekseyVY@gmail.com>
2021-11-06 12:02:53 +0300

    Update index.js

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,11 +6,9 @@ const path = require('path');
 const buildCSS = async () => {
   const files = await fs.promises.readdir(path.join(__dirname, 'styles'), {withFileTypes: true});
   const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), {flags: 'w'})
-  let template = []
   await Promise.all(files.map(file => new Promise(async (res) => {
       if(file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css') {
         const chunk = await fs.promises.readFile(path.join(__dirname, 'styles', file.name), {encoding: 'utf-8'})
-        template.push(chunk)
         writeStream.write(chunk);
         writeStream.write('\n');
         res();
