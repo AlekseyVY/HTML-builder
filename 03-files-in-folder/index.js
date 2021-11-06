4471443038ae7a593c4051cac7028d396bd3d3d6
Aleksey <AlekseyVY@gmail.com>
2021-11-06 11:23:03 +0300

    Update index.js

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,7 +6,7 @@ fs.readdir(path.join(__dirname, 'secret-folder'),{withFileTypes: true}, (err, fi
   files.forEach((file) => {
     fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
       if(!stats.isDirectory()) {
-        process.stdout.write(`${file.name.split('.')[0]} - ${path.extname(path.join(__dirname, 'secret-folder', file.name)).slice(1)} - ${stats.size}bytes\n`);
+        process.stdout.write(`${file.name.split('.')[0]} - ${path.extname(path.join(__dirname, 'secret-folder', file.name)).slice(1)} - ${stats.size} bytes\n`);
       }
     });
   });
