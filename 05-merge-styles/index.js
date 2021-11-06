7c3f59d3ac924afb8c8536136dcb0d91c97bd17f
Aleksey <AlekseyVY@gmail.com>
2021-11-06 11:46:00 +0300

    fixed script

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,20 @@
 const fs = require('fs');
 const path = require('path');
-const stream = require('node:stream');
-const readline = require('readline');
 
 
-const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), {flags: 'w'});
-
-fs.readdir(path.join(__dirname, 'styles'),{withFileTypes: true}, (err, files) => {
-  if (err) process.stderr.write(err);
-  files.forEach((file) => {
-    fs.stat(path.join(__dirname, 'styles', file.name), (err, stats) => {
-      if(!stats.isDirectory() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css') {
-        const inStream = fs.createReadStream(path.join(__dirname, 'styles', file.name));
-        const outStream = new stream;
-        const rl = readline.createInterface(inStream, outStream);
-        rl.on('line', function(line) {
-          writeStream.write(line);
-          writeStream.write('\n');
-        });
+const buildCSS = async () => {
+  const files = await fs.promises.readdir(path.join(__dirname, 'styles'));
+  let template = ''
+  await Promise.all(files.map(file => new Promise(async (res) => {
+    await fs.stat(path.join(__dirname, 'styles', file), async (err, stats) => {
+      if(!stats.isDirectory() && path.extname(path.join(__dirname, 'styles', file)) === '.css') {
+        const chunk = await fs.promises.readFile(path.join(__dirname, 'styles', file), {encoding: 'utf-8'})
+        template += chunk
+        res();
       }
     });
-  });
-});
+  })))
+  await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), template,{flags: 'w'})
+}
 
+buildCSS()
