330b7819ef8fb9e0a2f025f519bdc25eb403c2c1
Aleksey <AlekseyVY@gmail.com>
2021-11-06 12:07:50 +0300

    Update index.js

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -39,11 +39,9 @@ copyDir(src, dest);
 const buildCSS = async () => {
   const files = await fs.promises.readdir(path.join(__dirname, 'styles'), {withFileTypes: true});
   const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'), {flags: 'w'})
-  let template = []
   await Promise.all(files.map(file => new Promise(async (res) => {
       if(file.isFile() && path.extname(path.join(__dirname, 'styles', file.name)) === '.css') {
         const chunk = await fs.promises.readFile(path.join(__dirname, 'styles', file.name), {encoding: 'utf-8'})
-        template.push(chunk)
         writeStream.write(chunk);
         writeStream.write('\n');
         res();
